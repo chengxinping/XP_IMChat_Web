ec7e77896332074ccf9e1690f182e9f37aa00d70
XinPingCheng <972649353@qq.com>
2017-07-03 14:55:00 +0800

    完成第一个Restful接口

--- src/main/java/cn/xpcheng/xpimchat/web/push/service/AccountService.java
+++ src/main/java/cn/xpcheng/xpimchat/web/push/service/AccountService.java
@@ -2,9 +2,8 @@ package cn.xpcheng.xpimchat.web.push.service;
 
 import cn.xpcheng.xpimchat.web.push.bean.User;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 /**
  * @author 哎呦哥哥
@@ -24,6 +23,10 @@ public class AccountService {
     //POST 127.0.0.1/api/account/login
     @POST
     @Path("/login")
+    //传入
+    @Consumes(MediaType.APPLICATION_JSON)
+    //返回
+    @Produces(MediaType.APPLICATION_JSON)
     public User post() {
         User user = new User();
         user.setName("哎呦哥哥");
