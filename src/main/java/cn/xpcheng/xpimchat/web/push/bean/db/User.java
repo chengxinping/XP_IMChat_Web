e505724179feb3c2af03f677d994d39a2e173557
XinPingCheng <972649353@qq.com>
2017-07-12 22:07:10 +0800

    创建UserFollow类（User对应关系），并完善User类

--- src/main/java/cn/xpcheng/xpimchat/web/push/bean/db/User.java
+++ src/main/java/cn/xpcheng/xpimchat/web/push/bean/db/User.java
@@ -1,12 +1,15 @@
 package cn.xpcheng.xpimchat.web.push.bean.db;
 
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.*;
 
+import javax.persistence.CascadeType;
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 用户的Model 对应数据库
@@ -64,6 +67,21 @@ public class User {
     //最后一次收到消息的时间
     @Column
     private LocalDateTime lastReceiveAt = LocalDateTime.now();
+
+
+    //我关注人的列表
+    //对应数据库字段为TB_FOLLOW.originId
+    //定义为懒加载  默认加载User信息时  并不查询这个集合
+    @JoinColumn(name = "originId")
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<UserFollow> following = new HashSet<>();
+
+    //关注我的列表
+    @JoinColumn(name = "targetId")
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<UserFollow> followers = new HashSet<>();
 
     public String getId() {
         return id;
@@ -159,5 +177,21 @@ public class User {
 
     public void setLastReceiveAt(LocalDateTime lastReceiveAt) {
         this.lastReceiveAt = lastReceiveAt;
+    }
+
+    public Set<UserFollow> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Set<UserFollow> following) {
+        this.following = following;
+    }
+
+    public Set<UserFollow> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(Set<UserFollow> followers) {
+        this.followers = followers;
     }
 }
---
+++ src/main/java/cn/xpcheng/xpimchat/web/push/bean/db/UserFollow.java
@@ -0,0 +1,126 @@
+package cn.xpcheng.xpimchat.web.push.bean.db;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 用户关系Model  用于用户之间好友的实现
+ *
+ * @author 哎呦哥哥
+ *         Created by qq972 on 2017/7/12.
+ */
+@Entity
+@Table(name = "TB_USER_FOLLOW")
+public class UserFollow {
+    @Id
+    @PrimaryKeyJoinColumn
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(updatable = false, nullable = false)
+    private String id;
+
+    //定义发起人  比我我关注你  origin就是我
+    //多对一   ->可以关注很多人  每一次关注都是一条数据
+    // 一个user对应多个userFollow
+    //optional 不可选，必须存储  一条记录必须有一个被关注人
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "originId")  //定义关联表字段名为originId 对应User.id
+    private User origin;
+
+    //把这个列提取到model中
+    @Column(nullable = false, updatable = false, insertable = false)
+    private String originId;
+
+    //被关注人   你
+    //被关注人可以被很多人也可以被很多人关注
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "targetId")
+    private User target;
+
+    //把这个列提取到model中
+    @Column(nullable = false, updatable = false, insertable = false)
+    private String targetId;
+
+    //别名，对target的备注
+    @Column
+    private String alias;
+
+    //定义为创建时间戳 创建时写入
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createAt = LocalDateTime.now();
+
+    //定义为更新时间戳 更新时写入
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime uodateAt = LocalDateTime.now();
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(User origin) {
+        this.origin = origin;
+    }
+
+    public String getOriginId() {
+        return originId;
+    }
+
+    public void setOriginId(String originId) {
+        this.originId = originId;
+    }
+
+    public User getTarget() {
+        return target;
+    }
+
+    public void setTarget(User target) {
+        this.target = target;
+    }
+
+    public String getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(String targetId) {
+        this.targetId = targetId;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUodateAt() {
+        return uodateAt;
+    }
+
+    public void setUodateAt(LocalDateTime uodateAt) {
+        this.uodateAt = uodateAt;
+    }
+}
